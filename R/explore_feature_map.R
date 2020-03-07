2508f5879567d91bd8fb36547682cc21d71c7c46
Serg Pokrovskyy <neosoftlviv@gmail.com>
2020-03-07 11:46:17 -0800

    Dynamic font scaling based on feature count

--- R/explore_feature_map.R
+++ R/explore_feature_map.R
@@ -27,80 +27,81 @@ explore_feature_map <- function(df) {
         stop("Invalid dataframe specified")
     if (nrow(df) == 0)
         stop("Dataframe is empty")
-    
+
     # Subset dataframe features based on user selection
     df <- df[, purrr::map_lgl(df, is.numeric)]
-    
+
     grobs <- list()
     g <- 0
     features <- names(df)
-    
+    f_count <- length(features)
+
     if (length(features) == 0)
         stop("No numeric columns present in the dataframe")
-    
+
     for (i in features) {
         for (j in features) {
             g <- g + 1
             if (i == j) {
                 # Feature distribution plot
-                grobs[[g]] <- (ggplot(df) + 
+                grobs[[g]] <- (ggplot(df) +
                                geom_density(aes(x = !!sym(i)),
-                                            fill = '#ed6663', 
-                                            color = '#ed6663') + 
+                                            fill = '#ed6663',
+                                            color = '#ed6663') +
                                theme_void())
             }
             else {
                 if (which(features == i) > which(features == j)) {
                     # Features joint distribution plot
-                    grobs[[g]] <- (ggplot(df) + 
-                                   geom_point(aes(x = !!sym(i), 
+                    grobs[[g]] <- (ggplot(df) +
+                                   geom_point(aes(x = !!sym(i),
                                                   y = !!sym(j)),
                                               color = '#0f4c81',
-                                              size = 0.5) + 
+                                              size = 0.5) +
                                    theme_void())
                 }
                 else {
                     # Pearson correlation block
                     ij_corr <- round(cor(df[[i]], df[[j]]), 4)
-                    grobs[[g]] <- (ggplot() + 
-                                   annotate("text", 
-                                            x = 4, 
-                                            y = 25, 
-                                            size = 4, 
-                                            label = ij_corr) + 
+                    grobs[[g]] <- (ggplot() +
+                                   annotate("text",
+                                            x = 4,
+                                            y = 25,
+                                            size = 30 / f_count,
+                                            label = ij_corr) +
                                    theme_void())
                 }
             }
         }
-        
+
         # Add row name to the right
         g <- g + 1
-        grobs[[g]] <- (ggplot() + 
-                       annotate("text", 
-                                x = 4, 
-                                y = 25, 
-                                angle = 90, 
-                                size = 4, 
-                                label = i) + 
+        grobs[[g]] <- (ggplot() +
+                       annotate("text",
+                                x = 4,
+                                y = 25,
+                                angle = 90,
+                                size = 20 / f_count,
+                                label = i) +
                        theme_void())
     }
-    
+
     # Bottom captions row
     for (i in features) {
         g <- g + 1
-        grobs[[g]] <- (ggplot() + 
-                       annotate("text", 
-                                x = 4, 
-                                y = 25, 
-                                size = 4, 
-                                label = i) + 
+        grobs[[g]] <- (ggplot() +
+                       annotate("text",
+                                x = 4,
+                                y = 25,
+                                size = 20 / f_count,
+                                label = i) +
                        theme_void())
     }
-    
+
     # Arrange resulting grids
-    grid.arrange(grobs = grobs, 
-                 ncol = length(features) + 1, 
-                 nrow = length(features) + 1,
-                 widths = append(rep(50, length(features)), 20),
-                 heights =  append(rep(50, length(features)), 20))
+    grid.arrange(grobs = grobs,
+                 ncol = f_count + 1,
+                 nrow = f_count + 1,
+                 widths = append(rep(50, f_count), 20),
+                 heights =  append(rep(50, f_count), 20))
 }
