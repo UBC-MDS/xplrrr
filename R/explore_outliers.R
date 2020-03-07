184163dc89614bdeccffb11ab66c1dd675d5dea7
Furqan Khan <fkhan72@uwo.ca>
2020-03-07 02:58:11 -0800

    updated the explore_outliers function and its test

--- R/explore_outliers.R
+++ R/explore_outliers.R
@@ -6,7 +6,35 @@
 #' @export
 #'
 #' @examples
-#' explore_outliers(iris)
-explore_outliers <- function(data) {
+#' df <- data.frame('col1'= c(1, 2, 1.00, 3, -1, 100),
+#' 'col2'= c(3, 1 ,3, -2, 3, -1))
+#' explore_outliers(df)
+
+explore_outliers <- function(df) {
+  if (class(df) != 'data.frame'){
+    stop('input type must be data.frame')}
+  if(!all(unlist(lapply(df, is.numeric)))){
+    stop('entire data.frame must be numeric')}
+
+  df_outlier = data.frame(outlier_count= double())
+  df_name <- data.frame(name=names(df))
+  for (c in df){
+    m <- sum(c)/length(c)
+    sd <- (sum((m-c)**2)/length(c))**0.5
+    lower_bound <- m - 2*sd
+    upper_bound <- m + 2*sd
+    outlier_c = 0
+    for (n in c){
+      if  (n <= upper_bound && n >= lower_bound){
+      }
+      else{
+        outlier_c <- outlier_c + 1
+      }
+    }
+    de <- list(outlier_count = outlier_c)
+    df_outlier <- rbind(df_outlier,de)
+  }
+  row.names(df_outlier) <- df_name$name
+  return(df_outlier)
 
 }
