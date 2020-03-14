7cc80cbdd997543c818a5f5b17a0955abadea26f
Serg Pokrovskyy <neosoftlviv@gmail.com>
2020-03-14 15:35:30 -0700

    Added Ggplot2 output tests for explore_feature_map() function

--- tests/testthat/test-explore_feature_map.R
+++ tests/testthat/test-explore_feature_map.R
@@ -8,6 +8,13 @@ test_that("Check empty dataframe", {
   expect_error(explore_feature_map(data.frame()), "Dataframe is empty", fixed=TRUE)
 })
 
-test_that("Check empty dataframe", {
+test_that("Check empty no numeric columns", {
   expect_error(explore_feature_map(iris %>% select('Species')), "No numeric columns present in the dataframe", fixed=TRUE)
+})
+
+test_that("Check return plot structure", {
+  p <- explore_feature_map(iris)
+  expect_is(p, 'gtable') # is Ggplot object
+  expect_equal(length(p[[4]]), 5) # X axis
+  expect_equal(length(p[[3]]), 5) # Y axis
 })
