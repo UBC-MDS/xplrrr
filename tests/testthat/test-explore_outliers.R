4f03d829917199129fc0ac262f6185342912f842
Furqan Khan <fkhan72@uwo.ca>
2020-03-14 04:36:52 -0700

    added comments to test-explore_outliers

--- tests/testthat/test-explore_outliers.R
+++ tests/testthat/test-explore_outliers.R
@@ -1,7 +1,5 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
 
+# Checks if explore_outliers finds the outlier in `col1` of dataframe
 test_that("Check number of outliers", {
   df = data.frame('col1'= c(1, 2, 1.00, 3, -1, 100),
                   'col2'= c(3, 1 ,3, -2, 3, -1))
