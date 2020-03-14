d7558ff1627e9ec78226f2d0509918a9de41a865
Furqan Khan <fkhan72@uwo.ca>
2020-03-14 04:38:18 -0700

    added new test to test-explore_outliers to check input type

--- tests/testthat/test-explore_outliers.R
+++ tests/testthat/test-explore_outliers.R
@@ -1,4 +1,3 @@
-
 # Checks if explore_outliers finds the outlier in `col1` of dataframe
 test_that("Check number of outliers", {
   df = data.frame('col1'= c(1, 2, 1.00, 3, -1, 100),
@@ -7,4 +6,9 @@ test_that("Check number of outliers", {
 
   expect_identical(results$outlier_count[1], 1)
   expect_identical(results$outlier_count[2], 0)
+})
+
+# Checks if explore_outliers throws error if data.frame is not passed
+test_that("Function input must be data.frame", {
+  expect_error(explore_outliers(2))
 })
