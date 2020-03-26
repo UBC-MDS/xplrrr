8efc3f25121b08b25efd046b54f5e869594a5e73
Serg Pokrovskyy <neosoftlviv@gmail.com>
2020-03-26 15:55:22 -0700

    Turned a few '=' into '<-'

--- R/explore_outliers.R
+++ R/explore_outliers.R
@@ -10,7 +10,7 @@
 #'
 #' @examples
 #' df <- data.frame('col1'= c(1, 2, 1.00, 3, -1, 100),
-#' 'col2'= c(3, 1 ,3, -2, 3, -1))
+#'                  'col2'= c(3, 1 ,3, -2, 3, -1))
 #' explore_outliers(df, 2)
 
 explore_outliers <- function(df, std_range) {
@@ -19,14 +19,14 @@ explore_outliers <- function(df, std_range) {
 
   df <- data.frame(df[, map_lgl(df, is.numeric)])
   df <- na.omit(df)
-  df_outlier = data.frame(outlier_count= double())
+  df_outlier <- data.frame(outlier_count= double())
   df_name <- data.frame(name=names(df))
   for (c in df){
     m <- sum(c)/length(c)
     sd <- (sum((m-c)**2)/length(c))**0.5
     lower_bound <- m - std_range*sd
     upper_bound <- m + std_range*sd
-    outlier_c = 0
+    outlier_c <- 0
     for (n in c){
       if  (!(n <= upper_bound && n >= lower_bound)){
         outlier_c <- outlier_c + 1
